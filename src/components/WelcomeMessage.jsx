37410f9f94bc6cf43b85f720703e8045b9628bcb
gauthier <mysterandco@gmail.com>
2021-01-18 10:14:27 +0100

    Design responsive improved

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -6,7 +6,7 @@ class WelcomeMessage extends Component {
       return (
       <div className="title">
           <h1>Welcome on <span className="logo">FlickHouse&nbsp;Game</span></h1>
-          <p>You'll be asked a series of <span className="button-yes">Yes</span>&nbsp;or&nbsp;<span className="button-no">No</span> questions. <br /> Answer as many as you can in the allowed time !<br /> Good luck !</p>
+        <p>You'll be asked a series of <span className="button-yes">Yes</span>&nbsp;or&nbsp;<span className="button-no">No</span> questions. <br /> Answer as many as you can in the allowed time&nbsp;!<br /> <strong>Good luck&nbsp;!</strong></p>
       </div>
     );
   }
