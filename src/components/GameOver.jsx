f29a0024796af3582b5e2f1ba9b5c77fb05c9721
gauthier <mysterandco@gmail.com>
2021-01-18 10:40:21 +0100

    CountDown module component added, button reload added about gameover

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -5,12 +5,17 @@ class GameOver extends Component {
     super(props);
   }
 
+  refreshApp = () => {
+    window.location.reload(true);
+  };
+
   render() {
     return (
       <div className="wrap-gameover">
         <h6>GAME OVER !</h6>
         <h4> Your score is { this.props.score +' / 20'} </h4>
         <button className="button-share"> SHARE </button>
+        <button className="button-reload" onClick={this.refreshApp}> RETRY </button>
       </div>
     );
   }
